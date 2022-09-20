150a01777dd4a7abb029a7b3292bda3b796c8a4e
Paul C <colandreapaul@gmail.com>
2022-09-20 08:54:06 -0400

    Add base styles

--- components/DynamicIsland/DynamicIsland.tsx
+++ components/DynamicIsland/DynamicIsland.tsx
@@ -1,6 +1,6 @@
 export default function DynamicIsland() {
   return (
-    <div>
+    <div className="rounded-lg bg-black text-white text-xs grid cursor-pointer w-fit">
       <p>Island</p>
     </div>
   );
