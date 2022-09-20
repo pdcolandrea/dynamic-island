756e7dbe73f1c4a0dbc17d8cfd86f0eb1571717e
Paul C <colandreapaul@gmail.com>
2022-09-20 09:00:03 -0400

    Add classnames for conditional css

--- components/DynamicIsland/DynamicIsland.tsx
+++ components/DynamicIsland/DynamicIsland.tsx
@@ -1,6 +1,24 @@
-export default function DynamicIsland() {
+import classNames from "classnames";
+import { useState } from "react";
+
+interface DynamicIslandProps {
+  isOpen: boolean;
+}
+export default function DynamicIsland(props: DynamicIslandProps) {
+  const [isIslandOpen, setIslandOpen] = useState(false);
+
+  const onIslandClicked = () => {
+    setIslandOpen((isOpened) => !isOpened);
+  };
+
   return (
-    <div className="rounded-lg bg-black text-white text-xs grid cursor-pointer w-fit">
+    <div
+      className={classNames(
+        "rounded-lg bg-black text-white text-xs grid cursor-pointer w-fit",
+        isIslandOpen ? "items-start" : "items-center"
+      )}
+      onClick={onIslandClicked}
+    >
       <p>Island</p>
     </div>
   );
