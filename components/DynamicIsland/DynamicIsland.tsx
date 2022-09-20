b793d61b6798a876163aeb443e4c5f8544d730b3
Paul C <colandreapaul@gmail.com>
2022-09-20 09:07:59 -0400

    Working framer-motion body resizing

--- components/DynamicIsland/DynamicIsland.tsx
+++ components/DynamicIsland/DynamicIsland.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 interface DynamicIslandProps {
@@ -6,20 +7,34 @@ interface DynamicIslandProps {
 }
 export default function DynamicIsland(props: DynamicIslandProps) {
   const [isIslandOpen, setIslandOpen] = useState(false);
+  const variants = {
+    open: {
+      width: "300px",
+      height: "auto",
+      borderRadius: "20px",
+    },
+    closed: {
+      width: "96px",
+      height: "12px",
+      borderRadius: "99px",
+    },
+  };
 
   const onIslandClicked = () => {
     setIslandOpen((isOpened) => !isOpened);
   };
 
   return (
-    <div
+    <motion.div
       className={classNames(
         "rounded-lg bg-black text-white text-xs grid cursor-pointer w-fit",
         isIslandOpen ? "items-start" : "items-center"
       )}
+      animate={isIslandOpen ? "open" : "closed"}
+      variants={variants}
       onClick={onIslandClicked}
     >
-      <p>Island</p>
-    </div>
+      <p>Island {isIslandOpen}</p>
+    </motion.div>
   );
 }
